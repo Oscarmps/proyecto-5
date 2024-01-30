a0b2c4e48eda09f6d9fda2473b46f7a1809730ab
Oscarmps <oscarmezaperezxd@gmail.com>
2024-01-30 16:05:34 -0600

    primera-pagina

---
+++ pag-1/index.js
@@ -0,0 +1,12 @@
+function ir(){
+    var c=12345;
+    var u="oscarmezaperezxd@gmail.com";
+    if(document.form.password.value==c && document.form.login.value==u){
+     alert("Bienvenidos BIKERS")
+    window.location="index-2.html";
+    
+    } 
+    else{
+    alert("por favor ingrese, correo y nip correcto.");
+   } 
+}
